83817fe58016298c8a93ce0f035791134b74022c
E-was-here <emmeran.colot@ulb.ac.be>
2025-03-26 20:41:48 +0100

    labData push

--- matlab/part4/processData.m
+++ matlab/part4/processData.m
@@ -4,66 +4,87 @@ repNumber = 10;
 N = 4096;
 
 % Load the data
-[u1 y1] = ReadData('signal_2_1.mat', N, repNumber);
-[u2 y2] = ReadData('signal_2_2.mat', N, repNumber);
-[u3 y3] = ReadData('signal_2_3.mat', N, repNumber);
-[u4 y4] = ReadData('signal_2_4.mat', N, repNumber);
-[u5 y5] = ReadData('signal_2_5.mat', N, repNumber);
-[u6 y6] = ReadData('signal_2_6.mat', N, repNumber);
-[u7 y7] = ReadData('signal_2_7.mat', N, repNumber);
-[u8 y8] = ReadData('signal_2_8.mat', N, repNumber);
-[u9 y9] = ReadData('signal_2_9.mat', N, repNumber);
-[uA yA] = ReadData('signal_2_A.mat', N, repNumber);
+[u1 y1] = ReadData('rep_2_1.mat', N, repNumber);
+[u2 y2] = ReadData('rep_2_2.mat', N, repNumber);
+[u3 y3] = ReadData('rep_2_3.mat', N, repNumber);
+[u4 y4] = ReadData('rep_2_4.mat', N, repNumber);
+[u5 y5] = ReadData('rep_2_5.mat', N, repNumber);
+[u6 y6] = ReadData('rep_2_6.mat', N, repNumber);
+[u7 y7] = ReadData('rep_2_7.mat', N, repNumber);
+[u8 y8] = ReadData('rep_2_8.mat', N, repNumber);
+[u9 y9] = ReadData('rep_2_9.mat', N, repNumber);
+[uA yA] = ReadData('rep_2_A.mat', N, repNumber);
+
+u1 = u1(:, end);
+y1 = y1(:, end);
+u2 = u2(:, end);
+y2 = y2(:, end);
+u3 = u3(:, end);
+y3 = y3(:, end);
+u4 = u4(:, end);
+y4 = y4(:, end);
+u5 = u5(:, end);
+y5 = y5(:, end);
+u6 = u6(:, end);
+y6 = y6(:, end);
+u7 = u7(:, end);
+y7 = y7(:, end);
+u8 = u8(:, end);
+y8 = y8(:, end);
+u9 = u9(:, end);
+y9 = y9(:, end);
+uA = uA(:, end);
+yA = yA(:, end);
 
 % plot the DFT of the last period of the output
 figure('units','normalized','outerposition',[0 0 1 1]); % Full screen
 subplot(3, 4, 1);
-plot(abs(fft(y1(:, end))));
+plot(db(fft(y1(:, end))));
 title('output 1');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
 subplot(3, 4, 2);
-plot(abs(fft(y2(:, end))));
+plot(db(fft(y2(:, end))));
 title('output 2');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
 subplot(3, 4, 3);
-plot(abs(fft(y3(:, end))));
+plot(db(fft(y3(:, end))));
 title('output 3');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
 subplot(3, 4, 4);
-plot(abs(fft(y4(:, end))));
+plot(db(fft(y4(:, end))));
 title('output 4');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
 subplot(3, 4, 5);
-plot(abs(fft(y5(:, end))));
+plot(db(fft(y5(:, end))));
 title('output 5');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
 subplot(3, 4, 6);
-plot(abs(fft(y6(:, end))));
+plot(db(fft(y6(:, end))));
 title('output 6');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
 subplot(3, 4, 7);
-plot(abs(fft(y7(:, end))));
+plot(db(fft(y7(:, end))));
 title('output 7');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
 subplot(3, 4, 8);
-plot(abs(fft(y8(:, end))));
+plot(db(fft(y8(:, end))));
 title('output 8');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
 subplot(3, 4, 9);
-plot(abs(fft(y9(:, end))));
+plot(db(fft(y9(:, end))));
 title('output 9');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
 subplot(3, 4, 10);
-plot(abs(fft(yA(:, end))));
+plot(db(fft(yA(:, end))));
 title('output A');
 xlabel('Frequency (Hz)');
 ylabel('Amplitude');
@@ -71,7 +92,7 @@ ylabel('Amplitude');
 %% Question 4.4. about power
 
 % Calculate the power of the input signals
-powerIn = zeros(10, 1);
+powerIn = zeros(1, 10);
 powerIn(1) = mean(abs(u1).^2);
 powerIn(2) = mean(abs(u2).^2);
 powerIn(3) = mean(abs(u3).^2);
@@ -99,9 +120,16 @@ powerOut(10) = mean(abs(yA).^2);
 % plot the output power as a function of the input power
 
 figure;
-plot(powerIn, powerOut, 'o');
+plot(db(powerIn), db(powerOut));
+hold on;
+plot(db(powerIn), db(powerOut), 'o');
 xlabel('Input power');
 ylabel('Output power');
 title('Output power as a function of the input power');
+
+coeffs = polyfit(db(powerIn(1:2)), db(powerOut(1:2)), 1);
+fittedLine = polyval(coeffs, db(powerIn));
+plot(db(powerIn), fittedLine, 'k');
+legend('Data', 'Fitted Line');
 
 
